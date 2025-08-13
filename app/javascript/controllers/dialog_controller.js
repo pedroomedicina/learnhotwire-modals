63da3048c5c8c8df8acd90be8471b79cce2def2c
Pedro Medina <pedromedina190@gmail.com>
2025-08-13 14:58:47 -0400

    Cleaning up with the Dialog Close Event

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     connect() {
         this.element.showModal()
+        this.element.addEventListener("close", () => { this.element.remove() })
     }
 }
